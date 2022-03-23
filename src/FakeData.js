a0734643fece8be37ab9d559e3546b97a4ce58df
sameersahu142 <sameer678sahu@gmail.com>
2022-03-23 21:39:38 +0530

    first commit

---
+++ src/FakeData.js
@@ -0,0 +1,17 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Sameer",
+    username: "Sam",
+  },
+  {
+    id: 2,
+    name: "Leanne",
+    username: "Bret",
+  },
+  {
+    id: 3,
+    name: "Howell",
+    username: "Antonette",
+  },
+];
